d6b5525c7b53304fd44d9aaefc981f330a2954cc
石木洪 <13920096993@126.com>
2021-06-23 19:48:38 +0800

    antd

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
+import { Button } from 'antd';
+import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Button type="primary">antd点击</Button>
     </div>
   );
 }
