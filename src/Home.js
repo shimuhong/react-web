4e55c913f033c27bf870093881fa2e0a075630ed
shimuhong <13920096993@126.com>
2021-08-30 14:44:26 +0800

    建党100周年

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from "react";
 import './App.css';
 import src from "./images/100.png";
+import beian from "./images/beian.png";
+import { Footer } from "antd/lib/layout/layout";
 class Home extends Component {
 
   constructor ( props ) {
@@ -13,8 +15,12 @@ class Home extends Component {
       <Fragment>
         <div className="App">
           <img className="img" alt="is a images!" src={src} />
+          
         </div>
-        
+        <Footer className="footer">
+        <img alt="beian!" src={beian} />
+        津ICP备18003125号
+        </Footer>
       </Fragment>
     );
   }
